ba66bf33bd850d8767c48b78ed584683882f195b
Nikita Gandhewar <nikita.gandhewar@scriptuit.com>
2020-06-03 12:02:14 +0530

    - status scheduler code updated

--- src/main/java/com/vasten/cli/service/impl/DeploymentStatusScheduler.java
+++ src/main/java/com/vasten/cli/service/impl/DeploymentStatusScheduler.java
@@ -78,7 +78,7 @@ public class DeploymentStatusScheduler {
 	List<DeployStatus> deployStatusList = new ArrayList<DeployStatus>();
 
 	// @Scheduled()
-//	@Scheduled(cron = "0 0/1 * * * *")
+	@Scheduled(cron = "0 0/1 * * * *")
 //	@Scheduled(cron = "10 * * * * *")
 	public void statusScheduler() throws IOException, GeneralSecurityException {
 		LOGGER.info("in the deployment status update scheduler");

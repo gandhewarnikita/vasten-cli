fcf8c2d8c496810e7d060be534c72074515253fa
Nikita Gandhewar <nikita.gandhewar@scriptuit.com>
2020-06-10 15:33:28 +0530

    - create deployment code updated

--- src/main/java/com/vasten/cli/service/impl/DeploymentsServiceImpl.java
+++ src/main/java/com/vasten/cli/service/impl/DeploymentsServiceImpl.java
@@ -214,7 +214,7 @@ public class DeploymentsServiceImpl implements DeploymentsService {
 		}
 
 		try {
-			Thread.sleep(10000);
+			Thread.sleep(30000);
 		} catch (InterruptedException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
